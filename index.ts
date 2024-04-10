11097c6d54cbdf00a65f95632de331a4b57ed39c
bushra-code-github <161161926+bushra-code-github@users.noreply.github.com>
2024-04-11 01:56:38 +0500

    my first commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+//4th live session "number guessing game"
+#! /usr/bin/env node 
+
+import inquirer from "inquirer";
+// 1) computer will generate a random number
+// 2) user input for guessing number
+// 3) compare user input with computer generated number and shoe result
+const randomNumber =  Math.floor(Math.random() * 6 + 1);
+
+console.log(randomNumber);
+
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-6: ",
+    },
+]);
+
+if (answers.userGuessedNumber === randomNumber){
+    console.log("congratulations! you guessed right number.");
+      } 
+      else {
+        console.log (" you guessed wrong number.");
+    }
